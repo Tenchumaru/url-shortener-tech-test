e0d3931a17f3aeb5aa69f9cdef94b5879b65a83f
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2023-12-07 14:46:36 -0800

    Implement a multiprocessor system using diskcache

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
+import diskcache
+import secrets
 
 app = FastAPI()
+cache = diskcache.Cache()
 BASE_URL: str = "http://localhost:8000"
 
 
@@ -16,7 +19,16 @@ async def url_shorten(request: ShortenRequest):
     Given a URL, generate a short version of the URL that can be later resolved to the originally
     specified URL.
     """
-    short_url = "new-short-url"
+    while True:
+        short_url = cache.get(request.url)
+        if short_url:
+            break
+        short_url = secrets.token_urlsafe(4)
+        if cache.add(short_url, request.url):
+            if cache.add(request.url, short_url):
+                break
+            else:
+                cache.delete(short_url)
     return {"short_url": f"{BASE_URL}/r/{short_url}"}
 
 
@@ -30,7 +42,10 @@ async def url_resolve(short_url: str):
     Return a redirect response for a valid shortened URL string.
     If the short URL is unknown, return an HTTP 404 response.
     """
-    return RedirectResponse("http://original/url")
+    original_url = cache.get(short_url)
+    if original_url is None:
+        raise HTTPException(status_code=404, detail="URL not found")
+    return RedirectResponse(original_url)
 
 
 @app.get("/")
